8429e6a6884794a9feeb7e0a1dc802df2a464dee
Red Alpha <93113261+MhmdMukarram@users.noreply.github.com>
2022-02-26 07:32:38 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('ZIM_CODE' in process.env && string === undefined) {
+        if ('CHINTU_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Zim;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Chintu;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
